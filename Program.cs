61bae8d7bffc205978eaa610c63b42f22d77de5b
Jorge Luis <l20212419@tectijuana.edu.mx>
2021-03-10 12:31:06 -0800

    a
    a

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace persona
+{
+    class persona
+    {
+        private string _nombre;
+        private string _apellido;
+
+     
+
+        public string Nombre
+        {
+
+            get
+            {
+                return _nombre;
+            }
+
+            set
+            {
+                _nombre = value;
+            }
+        }
+
+
+        public string getNombre()
+        {
+            return _nombre;
+        }
+
+        //public string getNombre => _nombre;
+        //public string Nombre => _nombre;
+
+      
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            persona ana = new persona();
+            //ana.setNombre("Ana");
+            ana.Nombre = "Ana";
+            //Console.WriteLine($" Nombre: {ana.getNombre()}");
+            Console.WriteLine($" Nombre: {ana.Nombre}");
+
+            Console.ReadKey();
+        }
+    }
+}
